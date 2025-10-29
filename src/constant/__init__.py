c43abfb0442ba32d9429d7376c4a3b0ee86ed6f0
Anjali Jaiswal <anjalirjaiswal@gmail.com>
2025-10-29 12:39:47 +0530

    completed all modules

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -1,10 +1,10 @@
 AWS_S3_BUCKET_NAME = "wafer-fault"
-MONGO_DATABASE_NAME = "pwskills"
+MONGO_DATABASE_NAME = "sensor"
 MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URL="mongodb+srv://anjalirjaiswal_db_user:12345@cluster0.1g7ajta.mongodb.net/?appName=Cluster0"
 
 
 MODEL_FILE_NAME = "model"
